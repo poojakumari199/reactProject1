abb5decc1c9a33442ece9e1fcaedfb40a9054f09
Pooja Shrivastava <poo1234.kumari@gmail.com>
2022-02-24 17:38:58 +0530

    reactproject1

---
+++ src/components/UI/ErrorModel.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from './Card';
+import Button from './Button';
+import './ErrorModel.css';
+
+const ErrorModal = props => {
+return ( 
+    <div>
+        <div className="backdrop" onClick ={props.onConfirm} /> 
+<Card className="modal">
+    <header className="header">
+        <h2>{props.title}</h2>
+    </header>
+    <div className="content">
+        <p>{props.message}</p>
+    </div>
+    <footer className="actions">
+<Button onClick={props.onConfirm}>Ok</Button>
+    </footer>
+</Card>
+</div>
+);
+};
+
+export default ErrorModal;
